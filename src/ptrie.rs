01d781b5ba0e5f4b031d737b7043fc939a49fc6e
Francois Ripault <francois.ripault@epita.fr>
2013-07-29 15:49:28 +0200

    Add ptrie data-structure

---
+++ src/ptrie.rs
@@ -0,0 +1,20 @@
+
+/* Compiled representation of our Trie
+struct PTrie
+{
+  nb_succ : int,
+  key_len : int,
+  word_stop : int,
+  freq : int
+  key     : [char],
+  succ    : [int]
+}
+*/
+
+struct PTrie
+{
+  key       : ~str,
+  freq      : int,
+  word_stop : bool,
+  succ      : ~[Ptrie]
+}
