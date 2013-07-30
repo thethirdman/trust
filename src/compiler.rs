e805d7fe9299cf24cdf1a259bea5ece7bf9a13c5
Francois Ripault <francois.ripault@epita.fr>
2013-07-30 17:31:08 +0200

    Refactor main

--- src/compiler.rs
+++ src/compiler.rs
@@ -12,8 +12,52 @@ fn get_word(line : ~str) -> (~str, uint)
   match freq
   {
     None => fail!("Error: could not convert `" + words[1] + "` to an int"),
-    Some (ret) => (word.to_str(), ret)
+         Some (ret) => (word.to_str(), ret)
   }
+}
+
+fn make_trie(args : &[~str]) -> ~PTrie
+{
+  if (args.len() != 3)
+  {
+    fail!("Usage: " + args[0] + " /path/to/word/freq.txt /path/to/output/dict.bin")
+  }
+  else
+  {
+    let path    = PosixPath(args[1]);
+
+    match io::file_reader(&path)
+    {
+      Err (_) =>
+      {
+        fail!("Error: could not open file");
+      },
+      Ok (freader) =>
+      {
+        let lines    = freader.read_lines();
+        let mut it = lines.consume_iter().transform(|l| get_word(l));
+        let (w, f) = it.next().unwrap();
+        let mut trie = ~PTrie::new(w);
+        trie.freq = f;
+
+        for it.advance |(word, freq)|
+        {
+          trie.add_word(word, freq);
+        }
+        trie
+      }
+    }
+  }
+}
+
+fn serialize_trie(args : &[~str], trie : ~PTrie)
+{
+  let path          = PosixPath(args[2]);
+  let fwriter       = io::buffered_file_writer(&path).unwrap();
+  let serialization = trie.serialize();
+
+  for serialization.iter().advance |i|
+  { fwriter.write_le_u64((*i) as u64) }
 }
 
 // FIXME: exit comme un grand
@@ -21,53 +65,13 @@ fn get_word(line : ~str) -> (~str, uint)
 fn main()
 {
   let args : ~[~str] = os::args();
+  serialize_trie(args, make_trie(args));
+    // println("Num nodes: " + trie.len().to_str());
 
-  if (args.len() != 3)
-  {
-    fail!("Usage: " + args[0] + " /path/to/word/freq.txt /path/to/output/dict.bin");
-  }
-  else
-  {
-    let path    = PosixPath(args[1]);
-    let freader = io::file_reader(&path);
-
-    if freader.is_err()
     {
-      fail!("Error: could not open file");
     }
-    else
-    {
-      let reader   = freader.unwrap();
-      let lines    = reader.read_lines();
-      let mut it = lines.consume_iter().transform(|l| get_word(l));
-      let (w, f) = it.next().unwrap();
-      let mut trie = PTrie::new(w);
-      trie.freq = f;
-
-      let mut i: uint = 1;
-
-      for it.advance |(word, freq)|
-      {
-          trie.add_word(word, freq);
-          i = i + 1;
-          // println(i.to_str());
-      }
-
-      // println("Num nodes: " + trie.len().to_str());
-
-      {
-        let path          = PosixPath(args[2]);
-        let fwriter       = io::buffered_file_writer(&path).unwrap();
-        let serialization = trie.serialize();
-
-        for serialization.iter().advance |i|
-        { fwriter.write_le_u64((*i) as u64) }
-      }
-
-      println(trie.to_dot_str());
-    }
-  // Serialize
-  // Write
-  }
-
+    //println(trie.to_dot_str());
+    // Serialize
+    // Write
 }
+
